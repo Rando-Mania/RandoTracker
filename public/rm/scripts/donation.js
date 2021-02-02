8f31c9f96e328f061980f143bd7373730776c5a5
jsnkuhn <jsnkuhn@gmail.com>
2021-02-02 10:08:25 -0600

    undo auth

--- public/rm/scripts/donation.js
+++ public/rm/scripts/donation.js
@@ -3,18 +3,18 @@ async function fetchDonationData(){
     const progressEl = document.getElementById("progress-bar");
     let fakeDonation = 0;
 
-    const myInit = {
-        method: 'GET',
-        headers: {
-            'Access-Control-Allow-Headers': 'X-API-Key',
-            'Host': 'www.speedrun.com',
-            'Accept': 'application/json',
-            'X-API-Key': 'pzhp383omhr9ib37oc3kesqjh',
-          },
-        mode: 'cors'
-      };
+    // const myInit = {
+    //     method: 'GET',
+    //     headers: {
+    //         'Access-Control-Allow-Headers': 'X-API-Key',
+    //         'Host': 'www.speedrun.com',
+    //         'Accept': 'application/json',
+    //         'X-API-Key': 'pzhp383omhr9ib37oc3kesqjh',
+    //       },
+    //     mode: 'cors'
+    //   };
 
-    let response = await fetch('https://www.speedrun.com/api/v1/games/k6q4v49d/donations', myInit);
+    let response = await fetch('https://www.speedrun.com/api/v1/games/k6q4v49d/donations');
     let data = await response.json();
     let donationTotal = data.data['total-donated'];
     let adjustedTotal = donationTotal + fakeDonation;
