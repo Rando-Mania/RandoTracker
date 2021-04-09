16de5cd047c6a1120353518078bdecba76b2e3cf
jsnkuhn <jsnkuhn@gmail.com>
2021-04-08 19:32:13 -0500

    ugg js

---
+++ public/ff6wc/script/ff6wc.js
@@ -0,0 +1,86 @@
+(function(FF6) {
+
+// youtube iframe
+
+var tag = document.createElement('script');
+tag.id = 'iframe-demo';
+tag.src = 'https://www.youtube.com/iframe_api';
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('existing-iframe-example', {
+        events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange,
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    player.setLoop(true);
+    setTimeout( function() {
+        event.target.setShuffle(true);
+        player.playVideoAt(0);
+        player.stopVideo();
+    }, 3000);
+}
+
+function onPlayerStateChange(event) {
+    if (event.data == 0) { // video ended
+        player.setShuffle({'shufflePlaylist' : 1});
+    }
+}
+
+
+    // document.addEventListener('DOMContentLoaded', function() {
+
+    //     let dg = document.querySelector('[property="dragon-goal"] input');
+    //     let ds = document.querySelectorAll('.icon-slot.dragons');
+
+    //     function checkIfZero(){
+    //         if (this.value == 0) {
+    //             ds.forEach( function(item) {
+    //                 item.classList.add('display-none')
+    //             }) 
+    //         }
+    //         else {
+    //             ds.forEach( function(item) {
+    //                 item.classList.remove('display-none')
+    //             })
+    //         }
+    //     }
+
+    //     dg.addEventListener('change', checkIfZero);
+    
+    // });
+
+
+
+    document.addEventListener('DOMContentLoaded', function() {
+
+        setTimeout(function(){
+
+            // let dg = document.querySelector('[property="dragon-goal"] input');
+            let ds = document.querySelectorAll('.icon-slot.dragons');
+
+            Tracker.getLayoutData(function(data) {
+                
+                if (data["dragon-goal"] == 0) {
+                    ds.forEach( function(item) {
+                        item.classList.add('display-none')
+                    }) 
+                }
+                else {
+                    ds.forEach( function(item) {
+                        item.classList.remove('display-none')
+                    })
+                }
+            });
+
+        }, 3000);
+
+    });
+
+})(window.FF6 = window.FF6 || {});
