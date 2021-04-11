172a1173f94eab2eb15a0e085081db04fa0369a2
jsnkuhn <jsnkuhn@gmail.com>
2021-04-10 20:44:33 -0500

    1p character count

--- public/ff6wc/script/ff6wc.js
+++ public/ff6wc/script/ff6wc.js
@@ -1,104 +1,109 @@
 (function(FF6) {
 
-// youtube iframe
+    var youtubeIframe = function(){
+    // youtube iframe
 
-var tag = document.createElement('script');
-tag.id = 'iframe-demo';
-tag.src = 'https://www.youtube.com/iframe_api';
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+        var tag = document.createElement('script');
+        tag.id = 'iframe-demo';
+        tag.src = 'https://www.youtube.com/iframe_api';
+        var firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-var player;
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('existing-iframe-example', {
-        events: {
-        'onReady': onPlayerReady,
-        'onStateChange': onPlayerStateChange,
+        var player;
+        function onYouTubeIframeAPIReady() {
+            player = new YT.Player('existing-iframe-example', {
+                events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange,
+                }
+            });
         }
-    });
-}
 
-function onPlayerReady(event) {
-    player.setLoop(true);
-    setTimeout( function() {
-        event.target.setShuffle(true);
-        player.playVideoAt(0);
-        player.stopVideo();
-    }, 3000);
-}
+        function onPlayerReady(event) {
+            player.setLoop(true);
+            setTimeout( function() {
+                event.target.setShuffle(true);
+                player.playVideoAt(0);
+                player.stopVideo();
+            }, 3000);
+        }
 
-function onPlayerStateChange(event) {
-    if (event.data == 0) { // video ended
-        player.setShuffle({'shufflePlaylist' : 1});
+        function onPlayerStateChange(event) {
+            if (event.data == 0) { // video ended
+                player.setShuffle({'shufflePlaylist' : 1});
+            }
+        }
     }
-}
 
-
-    // document.addEventListener('DOMContentLoaded', function() {
-
-    //     let dg = document.querySelector('[property="dragon-goal"] input');
-    //     let ds = document.querySelectorAll('.icon-slot.dragons');
-
-    //     function checkIfZero(){
-    //         if (this.value == 0) {
-    //             ds.forEach( function(item) {
-    //                 item.classList.add('display-none')
-    //             }) 
-    //         }
-    //         else {
-    //             ds.forEach( function(item) {
-    //                 item.classList.remove('display-none')
-    //             })
-    //         }
-    //     }
-
-    //     dg.addEventListener('change', checkIfZero);
-    
-    // });
-
-
-
-    // document.addEventListener('DOMContentLoaded', function() {
-
-        // setTimeout(function(){
-
-            // let dg = document.querySelector('[property="dragon-goal"] input');
-            // let ds = document.querySelectorAll('.icon-slot.dragons');
-
-            // Tracker.getLayoutData(function(data) {
-                
-            //     if (data["dragon-goal"] == 0) {
-            //         ds.forEach( function(item) {
-            //             item.classList.add('display-none')
-            //         }) 
-            //     }
-            //     else {
-            //         ds.forEach( function(item) {
-            //             item.classList.remove('display-none')
-            //         })
-            //     }
-            // });
-
-        // }, 3000);
-
-    // });
+    youtubeIframe();
 
     FF6.init = function() {
-        Tracker.onUpdateLayout = function(data) {
-            let ds = document.querySelectorAll('.icon-slot.dragons');
+        document.addEventListener("tr-layout", function(e) {
+            if (!e.detail) {
+                return;
+            }
 
-            if (data["dragon-goal"] == 0) {
-                ds.forEach( function(item) {
-                    item.classList.add('display-none')
-                }) 
+            let data = e.detail;
+ 
+            // update dragon goal
+            var updateDragonGoal = function (){
+                let ds = document.querySelectorAll('.icon-slot.dragons');
+
+                var dragonGoal = data["dragon-goal"] || "0";
+
+                if (dragonGoal == "0") {
+                    ds.forEach( function(item) {
+                        item.classList.add('display-none')
+                    }) 
+                }
+                else {
+                    ds.forEach( function(item) {
+                        item.classList.remove('display-none')
+                    })
+                }
+                return data;
             }
-            else {
-                ds.forEach( function(item) {
-                    item.classList.remove('display-none')
-                })
+
+            updateDragonGoal();
+
+            // update player 1 character count
+            var characterCountOne = function (){
+
+                var characterArrayP1 = [
+                    data["__p1__character-terra"],
+                    data["__p1__character-locke"],
+                    data["__p1__character-cyan"],
+                    data["__p1__character-shadow"],
+                    data["__p1__character-edgar"],
+                    data["__p1__character-sabin"],
+                    data["__p1__character-celes"],
+                    data["__p1__character-strago"],
+                    data["__p1__character-relm"],
+                    data["__p1__character-setzer"],
+                    data["__p1__character-mog"],
+                    data["__p1__character-gau"],
+                    data["__p1__character-gogo"],
+                    data["__p1__character-umaro"]
+                ]
+
+                function checkForTrue(char) {
+                    return char == true;
+                  }
+
+                var characterArrayP1Filtered = characterArrayP1.filter(checkForTrue);
+                
+                Tracker.updateLayout(
+                    '__p1__player-count', 
+                    characterArrayP1Filtered.length
+                );
+
+                console.log(characterArrayP1Filtered.length)
+            
+                return data;
             }
-            return data;
-        }
+            
+            characterCountOne();
+        })
     }
 
 })(window.FF6 = window.FF6 || {});
