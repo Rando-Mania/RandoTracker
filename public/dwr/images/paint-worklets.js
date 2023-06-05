3acf0d219d370f108fa5c98ea735417d8b05d8b6
jsnkuhn <jsnkuhn@gmail.com>
2023-06-04 20:03:38 -0500

    randomania 1p live logo replacement

--- public/dwr/images/paint-worklets.js
+++ public/dwr/images/paint-worklets.js
@@ -43,3 +43,18 @@ class stampBorder {
     }
   }
   registerPaint("dw1Border", dw1Border);
+
+  class dw2Border {  
+    paint(ctx, geom) {
+      ctx.strokeStyle = '#000';
+      ctx.lineWidth = 4; 
+      ctx.strokeRect(2, 2, geom.width - 4, geom.height - 4);
+
+      ctx.fillStyle = '#fff';
+      ctx.fillRect(2, 1, geom.width - 4, geom.height - 2);
+      ctx.fillRect(1, 2, geom.width - 2, geom.height - 4);
+      ctx.clearRect(4, 3, geom.width - 8, geom.height - 6);
+      ctx.clearRect(3, 4, geom.width - 6, geom.height - 8);
+    }
+  }
+  registerPaint("dw2Border", dw2Border);
