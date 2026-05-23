76f1905fec02ddd5cd46c1d8a8ed6f66778e94ca
jsnkuhn <jsnkuhn@gmail.com>
2026-05-23 18:58:58 -0500

    ff4fe v5 2p menu auto fill/resize names

---
+++ public/ff4fev5/script/two_game.js
@@ -0,0 +1,40 @@
+// if ('paintWorklet' in CSS) {
+//     CSS.paintWorklet.addModule('../shared/images/generic-fallback-worklet.js');
+// }
+
+function playerDummyText() {
+    Tracker.updateLayoutMultiple([
+        {property: '__p1__player-name', value: 'Player 1 Player 1 Player 1'},
+        {property: '__p1__player-final', value: '1:23:45'},
+        {property: '__p1__toggle-speaker', value: 1},
+        {property: '__p1__toggle-winner', value: 1},
+        {property: '__p1__player-pronoun', value: '(She/Her)'},
+        {property: '__p1__player-record', value: '1-2'},
+
+        {property: '__p2__player-name', value: 'Player 2'},
+        {property: '__p2__player-final', value: '1:23:45'},
+        {property: '__p2__toggle-winner', value: 1},
+        {property: '__p2__toggle-speaker', value: 1},
+        {property: '__p2__player-pronoun', value: '(He/Him)'},
+        {property: '__p2__player-record', value: '1-2'},
+    ]);
+}
+function staffDummyText() {
+    Tracker.updateLayoutMultiple([
+        {property: 'commentator-name-1', value: 'CommanderLeonhart'},
+        {property: 'commentator-pronoun-1', value: '(They/Them)'},
+        {property: 'commentator-name-2', value: 'RowdyRoddySniper'},
+        {property: 'commentator-pronoun-2', value: '(Ze/Hir)'},
+        {property: 'restreamers', value: 'BeautyInDiscovery'},
+        {property: 'trackers', value: 'FirebirdLover'}
+    ]);
+}
+// tracker.init(){
+//     Tracker.getLayoutData(function(data) {
+//         if (data["11"] || data["12"] || data["13"] || data["14"] || data["15"] || data["16"]) {
+//             Tracker.updateLayout('objective-one-text','Have Kokkol forge Legend Sword with Adamant')
+//         } else {
+//             Tracker.updateLayout('objective-one-text','')
+//         }
+//     });
+// }
